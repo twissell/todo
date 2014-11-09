68af8bd4fee2966e09d528757d59f76f059b01df
twissell <avb5260@gmail.com>
2014-11-09 15:35:02 +0100

    added removeTodo functionality

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,18 +1,30 @@
 var todoApp = angular.module('TodoApp', []);
 
 todoApp.controller('TodoListCtrl', function($scope) {
+  
   $scope.todos = [
-    {'content': 'first todo'},
-    {'content': 'second todo'}
+    {
+      'content': 'first todo',
+      'completed': false
+    },
+    {
+      'content': 'second todo',
+      'completed': false
+    }
   ];
 
   $scope.addTodo = function() {
     var newTodo = {
-      'content': $scope.newTodo.trim()
+      'content': $scope.newTodo.trim(),
+      'completed': false
     }
 
    $scope.todos.push(newTodo);
+  };
 
+  $scope.removeTodo = function(todo) {
+    var todoIndex = $scope.todos.indexOf(todo);
+    $scope.todos.splice(todoIndex, 1);
   };
 
 });
