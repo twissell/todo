81370beb1c93c00ddc4140649ef53b0576cd8649
twissell <avb5260@gmail.com>
2014-11-15 18:10:07 +0100

    added karma.conf.js file, added jasmine karma and angular-mock.

--- tests/controllers_test.js
+++ tests/controllers_test.js
@@ -1,12 +1,12 @@
 describe('TodoListCtrl', function(){
 
-  beforEach(module('todoApp'));
+  beforeEach(module('TodoApp'));
   
   it('should create todo models with 2 todos', inject(function($controller) {
     var scope = {},
         ctrl = $controller('TodoListCtrl', {$scope:scope});
 
-    expect(scope.todos.length).toBe(2);
+    expect(scope.todos.length).toBe(3);
   
   }));
 
