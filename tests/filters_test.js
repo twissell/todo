935cb7e8a360400529e086acafb9f5e573fdeb36
twissell <avb5260@gmail.com>
2014-11-17 18:39:24 +0100

    added total filter test

--- tests/filters_test.js
+++ tests/filters_test.js
@@ -27,15 +27,27 @@ describe('todoAppFilters', function(){
   describe('state filter', function(){
     
     it('should return all todos', function(){ 
-      expect(filter('state')(todos, 'all').length).toBe(3)  
+      expect(filter('state')(todos, 'all').length).toBe(3);  
     });
 
     it('should return just active todos', function(){
-      expect(filter('state')(todos, 'active').length).toBe(2)
+      expect(filter('state')(todos, 'active').length).toBe(2);
     });
 
     it('should return just completed todos', function(){
-      expect(filter('state')(todos, 'completed').length).toBe(1)
+      expect(filter('state')(todos, 'completed').length).toBe(1);
+    });
+
+  });
+
+  describe('total filter', function(){
+    
+    it('should return the total count of all active todos', function(){
+      expect(filter('total')(todos, 'active')).toBe(2);
+    });
+
+    it('should return the total count of all completed todos', function(){
+      expect(filter('total')(todos, 'completed')).toBe(1);
     });
 
   });
