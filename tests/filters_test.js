56628c8ec076d6e0f6ecba17d965326fa4e0975a
twissell <avb5260@gmail.com>
2014-11-17 18:31:37 +0100

    added test for state filter

---
+++ tests/filters_test.js
@@ -0,0 +1,43 @@
+describe('todoAppFilters', function(){
+
+  // http://stackoverflow.com/questions/21094569/how-to-unit-test-a-filter-in-angularjs
+
+  var filter;
+
+  var todos = [
+    {
+      'content': 'first todo',
+      'completed': false
+    }, 
+    {
+      'content': 'second todo',
+      'completed': true
+    },
+    {
+      'content': 'third todo',
+      'completed': false
+    }
+  ]
+
+  beforeEach(module('todoAppFilters'));
+  beforeEach(inject(function($filter){
+    filter = $filter;
+  }));
+
+  describe('state filter', function(){
+    
+    it('should return all todos', function(){ 
+      expect(filter('state')(todos, 'all').length).toBe(3)  
+    });
+
+    it('should return just active todos', function(){
+      expect(filter('state')(todos, 'active').length).toBe(2)
+    });
+
+    it('should return just completed todos', function(){
+      expect(filter('state')(todos, 'completed').length).toBe(1)
+    });
+
+  });
+
+});
